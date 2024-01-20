ff1456bc63b0d66f1421e65b4656d225becffb0e
Ryan England <me@rengland.org>
2024-01-20 12:48:35 -0800

    Added error handling

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
@@ -16,7 +17,7 @@ func Home(w http.ResponseWriter, r *http.Request) {
 
 func About(w http.ResponseWriter, r *http.Request) {
 	sum := AddValue(2, 2)
-	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum))
+	fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum))
 }
 
 // Uppercase function names make them public
@@ -25,11 +26,31 @@ func AddValue(x, y int) int {
 	return x + y
 }
 
+func Divide(w http.ResponseWriter, r *http.Request) {
+	f, err := divideValues(100.0, 0.0)
+	if err != nil {
+		fmt.Fprintf(w, fmt.Sprintf("%s", err))
+		return
+	}
+
+	fmt.Fprintf(w, fmt.Sprintf("%f / %f = %f", 100.0, 10.0, f))
+}
+
+func divideValues(x, y float32) (float32, error) {
+	if y <= 0 {
+		err := errors.New("Cannot divide by zero!")
+		return 0, err
+	}
+	result := x / y
+	return result, nil
+}
+
 // Lowercase function names make them private
 // to this specific module
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
+	http.HandleFunc("/divide", Divide)
 
 	_, _ = fmt.Println(fmt.Sprintf("Starting applicatiton on port %s", portNumber))
 
